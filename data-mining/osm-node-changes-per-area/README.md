f97e287737c408ab91541a3bd9632c7b0542561c
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2016-12-28 01:56:56 +0100

    data-mining/osm-node-changes-per-area introduced

---
+++ data-mining/osm-node-changes-per-area/README.md
@@ -0,0 +1,31 @@
+# osm-vis/osm-node-changes-per-area
+
+The tool extracts the changes on nodes from [OSM replication diffs)[https://planet.openstreetmap.org/replication/day/] and aggregates them by space and time.
+
+## Installation
+
+To install the tool, [Haskell](https://www.haskell.org/platform/) needs to be installed. Then execute:
+```
+mkdir osm-vis && cd osm-vis
+wget https://github.com/mocnik-science/osm-vis/archive/master.zip
+unzip master.zip
+cd osm-vis/data-mining/osm-node-changes-per-area
+cabal sandbox init
+cabal update
+cabal install
+```
+
+## Usage
+
+To fetch and process the data, execute:
+```
+cabal run
+```
+
+## Author
+
+This application is written and maintained by Franz-Benjamin Mocnik, <mail@mocnik-science.net>.
+
+(c) by Franz-Benjamin Mocnik, 2016.
+
+The code is licensed under the [GPL-3](https://github.com/mocnik-science/osm-vis/blob/master/data-mining/osm-tags-history-wiki-data/LICENSE.md).
---
+++ data-mining/osm-node-changes-per-area/osm-node-changes-per-area.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Chorale.Common
+import qualified Codec.Compression.GZip as GZip
+import Control.Monad
+import Data.Aeson hiding (Result)
+import qualified Data.ByteString as BB
+import qualified Data.ByteString.Char8 as CC
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as C
+import Data.List
+import Data.Maybe
+import Data.Time
+import GHC.Generics
+import Network.Curl.Download
+
+import Network.Curl.Download.Lazy
+import Safe
+import Text.XML.Hexml
+
+urlState :: String
+urlState = "https://planet.openstreetmap.org/replication/day/state.txt"
+urlFile :: Int -> String
+urlFile n = "https://planet.openstreetmap.org/replication/day/" ++ (justifyRight 3 '0' . show) (div n 1000000) ++ "/" ++ (justifyRight 3 '0' . show) (mod (div n 1000) 1000) ++ "/" ++ (justifyRight 3 '0' . show) (mod n 1000) ++ ".osc.gz"
+outputFile :: String
+outputFile = "../../data/osm-node-changes-per-area.json"
+
+-- --== MAIN
+
+main :: IO ()
+main = do
+    (latestTimestamp', url', nodeChanges') <- downloadAndParseXml
+    let nodeChangesStatistics = aggregateNodeChanges . map roundNodeChange $ nodeChanges'
+    C.writeFile outputFile . encode $ Result latestTimestamp' "Statistics on node changes of OpenStreetMap data" "OpenStreetMap project, <a href=\"http://opendatacommons.org/licenses/odbl/\" target=\"_blank\">Open Data Commons Open Database License (ODbL)</a>" url' nodeChangesStatistics
+
+-- --== DOWNLOAD FILES
+
+downloadAndParseXml :: IO (String, String, [NodeChangeUTCTime])
+downloadAndParseXml = do
+    info <- lines . either (error . ("[ERROR]" ++)) id <$> openURIString urlState
+    let latestSequenceNumber = nothingToError "[ERROR] Could not read sequence number" . join . mapJust readMay . headMay . mapMaybe (stripPrefix "sequenceNumber=") $ info
+    let latestTimestamp = nothingToError "[ERROR] Could not read timestamp" . mapJust (replaceElementInList '\\' "") . headMay . mapMaybe (stripPrefix "timestamp=") $ info
+    nodeChanges' <- parseChangesXml . GZip.decompress . leftToError "[ERROR] Could not read file" <$> (openLazyURI . urlFile) latestSequenceNumber
+    return (latestTimestamp, urlFile latestSequenceNumber, nodeChanges')
+
+-- --== RESULT
+
+data Result = Result {timestmap :: String, description :: String, source :: String, url :: String, nodeChanges :: [NodeChangeStatistics]} deriving Generic
+
+instance ToJSON Result where
+    toEncoding = genericToEncoding defaultOptions
+
+-- --== NODECHANGE
+
+data NodeChangeUTCTime = NodeChangeUTCTime UTCTime Double Double deriving Show
+data NodeChange = NodeChange String Double Double deriving (Show, Eq, Ord)
+data NodeChangeStatistics = NodeChangeStatistics {timestamp :: String, lat :: Double, lon :: Double, count :: Int} deriving (Generic, Show)
+
+instance ToJSON NodeChangeStatistics where
+    toEncoding = genericToEncoding defaultOptions
+
+-- --== PARSE XML
+
+parseChangesXml :: B.ByteString -> [NodeChangeUTCTime]
+parseChangesXml = mapMaybe parseChangeXml . flip childrenByDeep "node" . leftToError "[ERROR] Could not parse file" . parse . C.toStrict
+
+childrenByDeep :: Node -> BB.ByteString -> [Node]
+childrenByDeep node s = childrenBy node s ++ concatMap (`childrenByDeep` s) (children node)
+
+parseChangeXml :: Node -> Maybe NodeChangeUTCTime
+parseChangeXml node = if isJust timestamp' && all isJust [lat', lon']
+    then Just $ NodeChangeUTCTime (fromJust timestamp') (fromJust lat') (fromJust lon')
+    else Nothing where
+        timestamp' = parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%S%Z" . CC.unpack . attributeValue =<< attributeBy node "timestamp"
+        lat' = readMay . CC.unpack. attributeValue =<< attributeBy node "lat"
+        lon' = readMay . CC.unpack. attributeValue =<< attributeBy node "lon"
+
+-- --== AGGREGATE NODECHANGES
+
+roundNodeChange :: NodeChangeUTCTime -> NodeChange
+roundNodeChange (NodeChangeUTCTime timestamp' lat' lon') = NodeChange ((\(h, m) -> h ++ ":" ++ m) . map12 (justifyRight 2 '0' . show) . map21 (todHour, (* 10) . flip div 10 . todMin) . timeToTimeOfDay . utctDayTime $ timestamp') (fromIntegral . (round :: Double -> Int) $ lat') (fromIntegral . (round :: Double -> Int) $ lon')
+
+aggregateNodeChanges :: [NodeChange] -> [NodeChangeStatistics]
+aggregateNodeChanges = map ((\(NodeChange timestamp' lat' lon', n) -> NodeChangeStatistics timestamp' lat' lon' n) . map21 (head, length)) . sortAndGroup
+
+-- --== HELPERS
+
+nothingToError :: String -> Maybe a -> a
+nothingToError msg = \case
+    Just a -> a
+    Nothing -> error msg
+
+leftToError :: String -> Either a b -> b
+leftToError msg = leftToError' (const msg)
+
+leftToError' :: (a -> String) -> Either a b -> b
+leftToError' msg = \case
+    Left a -> error . msg $ a
+    Right b -> b
