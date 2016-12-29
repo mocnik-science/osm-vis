96a3291f3145c2bb86a94570e2bad246378e6bce
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2016-12-29 22:14:00 +0100

    data-mining/osmstats introduced

---
+++ data-mining/osmstats/run.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const URL = 'http://blackadder.dev.openstreetmap.org/OSMStats/osmstats20160916.csv'
+const DATA_PATH = '../../data/osmstats'
+const DATA_FILE = 'osmstats.json'
+
+const execSync = require('child_process').execSync
+const request = require('sync-request')
+const jsonfile = require('jsonfile')
+const moment = require('moment')
+
+execSync(`mkdir -p ${DATA_PATH}`)
+const contentCsv = request('GET', URL).getBody('utf-8')
+jsonfile.writeFileSync(`${DATA_PATH}/${DATA_FILE}`, {
+  dataTimestamp: moment().format('YYYY-MM-DDTHH:mm:ssZ'),
+  dataDescription: 'OSM statistics',
+  dataSource: 'OSMStats; OpenStreetMap project, <a href=\"http://opendatacommons.org/licenses/odbl/\" target=\"_blank\">ODbL</a>',
+  dataUrl: URL,
+  data: contentCsv,
+})
