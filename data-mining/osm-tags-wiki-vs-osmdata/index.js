e01d877feebe5139a8fd1b87631801020f2ef3d0
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2017-02-02 22:23:05 +0100

    data-mining/osm-tags-wiki-vs-osmdata: code cleanup; bugs

--- data-mining/osm-tags-wiki-vs-osmdata/index.js
+++ data-mining/osm-tags-wiki-vs-osmdata/index.js
@@ -4,43 +4,42 @@ const fs = require('fs')
 
 const wiki = require('../../data/osm-tags-history-wiki.json')
 
-var out = {
-    dataSource: 'OpenStreetMap project, wiki content © <a href="http://wiki.openstreetmap.org/wiki/Wiki_content_license" target="_blank">CC BY-SA 2.0</a>, data © <a href="http://openstreetmap.org/copyright" target="_blank">ODbL</a>',
-    dataDescription: 'Comparison between first occurence of OSM tags in OpenStreetMap\'s data and wiki',
-    dataTimestamp: (new Date()).toISOString(),
-    dataUrl: 'http://taghistory.raifer.tech',
-    data: null
-}
-
-out.data = wiki.descriptionHistory.map(wikipage => ({
+const out = {
+  dataSource: 'OpenStreetMap project, wiki content © <a href="http://wiki.openstreetmap.org/wiki/Wiki_content_license" target="_blank">CC BY-SA 2.0</a>, data © <a href="http://openstreetmap.org/copyright" target="_blank">ODbL</a>',
+  dataDescription: 'Comparison between first occurence of OSM tags in OpenStreetMap\'s data and wiki',
+  dataTimestamp: (new Date()).toISOString(),
+  dataUrl: 'http://taghistory.raifer.tech',
+  data: wiki.descriptionHistory.map(wikipage => ({
     key: wikipage.key,
     value: wikipage.value,
-    'date-wiki': wikipage.history[0][0].substr(0,10),
+    'date-wiki': wikipage.history[0][0].substr(0, 10),
     'date-data': null,
-    count: null
-}))
+    count: null,
+  })),
+}
 
-const miningQueue = queue(3) // 3 concurrent http requests should be ok
+const miningQueue = queue(3)
 
-wiki.descriptionHistory.forEach(function(wikipage) {
-    miningQueue.defer(function(callback) {
-        fetch('http://taghistory.raifer.tech/***/'+encodeURIComponent(wikipage.key)+'/'+encodeURIComponent(wikipage.value))
-            .then(response => response.json())
-            .then(function(json) {
-                callback(null, {
-                    date: json[0].date.substr(0,10),
-                    count: json[json.length-1].count
-                })
-            }).catch(callback)
-    })
+out.data.forEach(wikipage => {
+  miningQueue.defer(callback => {
+    fetch(`http://taghistory.raifer.tech/***/${encodeURIComponent(wikipage.key)}${(wikipage.value !== '*') ? '/' + encodeURIComponent(wikipage.value) : ''}`)
+      .then(response => response.json())
+      .then(json => {
+        if (json.length == 0 || json[0].date == undefined) callback(null)
+        callback(null, {
+            date: json[0].date.substr(0, 10),
+            count: json[json.length - 1].count,
+        })
+    }).catch(callback)
+  })
 })
 
-miningQueue.awaitAll(function(err, results) {
-    if (err) return console.error(err)
-    results.forEach(function(result, index) {
-        out.data[index]['date-data'] = result.date
-        out.data[index].count = result.count
-    })
-    fs.writeFileSync('../../data/osm-tags-wiki-vs-osmdata.json', JSON.stringify(out))
+miningQueue.awaitAll((err, results) => {
+  if (err) return console.error(err)
+  results.forEach((result, index) => {
+    if (!result) return
+    out.data[index]['date-data'] = result.date
+    out.data[index].count = result.count
+  })
+  fs.writeFileSync('../../data/osm-tags-wiki-vs-osmdata.json', JSON.stringify(out))
 })
-
