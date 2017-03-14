50c955c80a731968df309c7052ac0ed661f85255
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2017-03-15 00:16:54 +0100

    data commited to repository (test)

--- bin/data-mining-daily.sh
+++ bin/data-mining-daily.sh
@@ -11,5 +11,17 @@ cd data-mining/osmstats
 ./run.js
 cd ../..
 
+# commit to repository
+DATE=`date +%Y-%m-%d`
+PATH_DAY="../tmp/daily/$DATE/"
+mkdir -p $PATH_DAY
+tar -zcvf $PATH_DAY/osm-node-changes-per-area.json.tar.gz data/osm-node-changes-per-area.json
+tar -zcvf $PATH_DAY/osmstats.json.tar.gz data/osmstats.json
+cd ../tmp
+git add *
+git commit -m "daily@$DATE"
+git push -u origin master
+cd ../osm-vis
+
 # post-merge hook
 .git/hooks/post-merge
--- bin/data-mining-monthly.sh
+++ bin/data-mining-monthly.sh
@@ -21,5 +21,19 @@ cd data-mining/osm-tags-word-frequency-wiki
 cabal run
 cd ../..
 
+# commit to repository
+DATE=`date +%Y-%m-%d`
+PATH_DAY="../tmp/monthly/$DATE/"
+mkdir -p $PATH_DAY
+tar -zcvf $PATH_DAY/naturalearth_ne_110m_admin_0_countries.topojson.tar.gz data/naturalearth_ne_110m_admin_0_countries.topojson
+tar -zcvf $PATH_DAY/osm-tags-history-wiki.json.tar.gz data/osm-tags-history-wiki.json
+tar -zcvf $PATH_DAY/osm-tags-wiki-vs-osmdata.json.tar.gz data/osm-tags-wiki-vs-osmdata.json
+tar -zcvf $PATH_DAY/osm-tags-word-frequency-wiki.json.tar.gz data/osm-tags-word-frequency-wiki.json
+cd ../tmp
+git add *
+git commit -m "monthly@$DATE"
+git push -u origin master
+cd ../osm-vis
+
 # post-merge hook
 .git/hooks/post-merge
