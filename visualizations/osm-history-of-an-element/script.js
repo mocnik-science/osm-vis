54d6a4578ad4b8cf5808896c7caffcea583e014e
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2017-07-05 20:43:20 +0200

    minor bugs

--- visualizations/osm-history-of-an-element/script.js
+++ visualizations/osm-history-of-an-element/script.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
   const featureToTime = f => moment(f.properties.timestamp)
   
   // map
-  const map = L.map('map', {zoomControl: false, zoomSnap: 0, scrollWheelZoom: false, touchZoom: false, dragging: false})
+  const map = L.map('map', {zoomControl: false, zoomSnap: 0, scrollWheelZoom: false, touchZoom: false, boxZoom: false, doubleClickZoom: false, dragging: false})
   new L.StamenTileLayer('toner').setOpacity(.14).addTo(map)
   mapPreventDrag('timeslider')
   
